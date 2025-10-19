6fa0005f416abb974cc013ccbf053712eef4aee3
Alex Menendez <amenendez15@usf.edu>
2025-10-19 17:02:50 -0400

    forgot to commit. made funtions for steps 3-6

--- cp2.js
+++ cp2.js
@@ -1,11 +1,41 @@
-const idk = new Promise (function(resolve, reject)) {
-    console.log("Checking...");
-    setTimeout(() => {
-        const success = true; // Simulate success or failure
-        if (success) {
-            resolve("Operation successful!");
-        } else {
-            reject("Operation failed.");
-        },3000);
-    };
-);  
+function fetchProductsThen() {
+  fetch("https://www.course-api.com/javascript-store-products")
+    .then(res => res.json())     // Converts response to JSON
+    .then(data => {
+      console.log("Products:", data);
+        displayProducts(data);
+    })
+    .catch(err => {
+      console.error("Fetch failed:", err);
+    });
+}
+
+
+const fetchProductsAsync = async function () {
+    try {
+        const res = await fetch("https://www.course-api.com/javascript-store-products");
+        if (!res.ok) {
+            throw new Error(`HELP ME`)
+        }
+        const data = await res.json();
+        console.log("Products:", data);
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+
+
+
+function displayProducts(products) {
+    products.slice(0,5).forEach(product => {
+        console.log(`${product.fields.name}, ${(product.fields.price / 100).toFixed(2)}`);
+    })
+};
+let handleError = (error) => {
+    console.error("An error occured:", error);
+}
+
+fetchProductsThen();
+fetchProductsAsync();
+
